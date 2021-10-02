a82a0ed9fa47d7c8fa58fc8cb18284cb68a72778
Vinurachan <vinura.18@itfac.mrt.ac.lk>
2021-10-02 21:07:41 +0530

    initialization

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const crypto = require('crypto')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.json(), cors())
+app.options('*', cors());
+
+app.post('/', (req, res) => {
+
+  const timestamp = new Date().getTime() - 30000
+  const msg = Buffer.from(process.env.ZOOM_JWT_API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64')
+  const hash = crypto.createHmac('sha256', process.env.ZOOM_JWT_API_SECRET).update(msg).digest('base64')
+  const signature = Buffer.from(`${process.env.ZOOM_JWT_API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
+
+  res.json({
+    signature: signature
+  })
+})
+
+app.listen(port, () => console.log(`zoom-signature-server running on port ${port}!`))
